46c868bb44cc06c2ef76eee7b7f6bcf7be9a3e57
Joseph <user.example.com>
2019-02-27 23:20:44 +0000

    DI - setup context module

---
+++ app/src/main/java/com/challenge/albumlistapp/di/modules/ContextModule.java
@@ -0,0 +1,14 @@
+package com.challenge.albumlistapp.di.modules;
+
+import android.app.Application;
+import android.content.Context;
+
+import dagger.Binds;
+import dagger.Module;
+
+@Module
+public abstract class ContextModule {
+
+    @Binds
+    abstract Context provideContext(Application application);
+}

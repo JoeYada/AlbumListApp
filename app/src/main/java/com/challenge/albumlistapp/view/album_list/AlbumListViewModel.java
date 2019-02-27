8f22fbb27ea648a9a33805c9d86003b6d9433a0c
Joseph <user.example.com>
2019-02-27 23:40:18 +0000

    Add AlbumListViewModel

---
+++ app/src/main/java/com/challenge/albumlistapp/view/album_list/AlbumListViewModel.java
@@ -0,0 +1,13 @@
+package com.challenge.albumlistapp.view.album_list;
+
+import javax.inject.Inject;
+
+import androidx.lifecycle.ViewModel;
+
+public class AlbumListViewModel extends ViewModel {
+
+    @Inject
+    public AlbumListViewModel() {
+    }
+
+}

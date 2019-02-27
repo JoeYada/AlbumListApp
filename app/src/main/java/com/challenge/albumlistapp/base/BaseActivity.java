311df7892cefc12589054f50825441865303619e
Joseph <user.example.com>
2019-02-27 20:24:19 +0000

    Add implementation for BaseActivity

--- app/src/main/java/com/challenge/albumlistapp/base/BaseActivity.java
+++ app/src/main/java/com/challenge/albumlistapp/base/BaseActivity.java
@@ -1,6 +1,21 @@
 package com.challenge.albumlistapp.base;
 
+import android.os.Bundle;
+
+import androidx.annotation.LayoutRes;
+import androidx.annotation.Nullable;
+import butterknife.ButterKnife;
 import dagger.android.support.DaggerAppCompatActivity;
 
-public class BaseActivity extends DaggerAppCompatActivity {
+public abstract class BaseActivity extends DaggerAppCompatActivity {
+
+    @LayoutRes
+    protected abstract int layoutRes();
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(layoutRes());
+        ButterKnife.bind(this); 
+    }
 }

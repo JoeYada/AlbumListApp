9a06f9b37aa28c13a5bf319eced3582008e57e5b
Joseph <user.example.com>
2019-02-27 20:47:47 +0000

    Add BaseApplication

---
+++ app/src/main/java/com/challenge/albumlistapp/base/BaseApplication.java
@@ -0,0 +1,17 @@
+package com.challenge.albumlistapp.base;
+
+import dagger.android.AndroidInjector;
+import dagger.android.support.DaggerApplication;
+
+public class BaseApplication extends DaggerApplication {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+
+    @Override
+    protected AndroidInjector<? extends DaggerApplication> applicationInjector() {
+        return null;
+    }
+}

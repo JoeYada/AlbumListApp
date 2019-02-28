67177ff6223dfc028e225e11ec2523fc8d273909
Joseph <user.example.com>
2019-02-28 12:25:18 +0000

    Add Connectivity checker to check whether user is online

---
+++ app/src/main/java/com/challenge/albumlistapp/utils/ConnectivityUtils/OnlineChecker.java
@@ -0,0 +1,9 @@
+package com.challenge.albumlistapp.utils.ConnectivityUtils;
+
+/**
+ * Simple interface that contains online/offline state indicator
+ */
+
+public interface OnlineChecker {
+    boolean isOnline();
+}

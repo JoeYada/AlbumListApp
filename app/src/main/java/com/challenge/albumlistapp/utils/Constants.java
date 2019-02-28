00cf76753fb21d9f3da8fa6ae2694acb9a9202c0
Joseph <user.example.com>
2019-02-28 11:44:33 +0000

    Add SchedulerProvider classes and DB name and version

--- app/src/main/java/com/challenge/albumlistapp/utils/Constants.java
+++ app/src/main/java/com/challenge/albumlistapp/utils/Constants.java
@@ -4,4 +4,6 @@ public class Constants {
 
     public static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
     public static final String RELATIVE_URL = "/albums";
+    public static final int DATABASE_VERSION = 1;
+    public static final String ALBUM_DB_NAME = "Albums.db";
 }

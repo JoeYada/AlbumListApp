044645f8202a60ac4259412a90bcc2fa6e70e620
Joseph <user.example.com>
2019-02-28 12:27:31 +0000

    Fetch data from remote once. Save the data to the local DB so that it can be retrieved locally when the user access the app

--- app/src/main/java/com/challenge/albumlistapp/service/remote/DataManagerImpl.java
+++ app/src/main/java/com/challenge/albumlistapp/service/remote/DataManagerImpl.java
@@ -2,24 +2,51 @@ package com.challenge.albumlistapp.service.remote;
 
 import com.challenge.albumlistapp.models.Album;
 import com.challenge.albumlistapp.service.AlbumsService;
+import com.challenge.albumlistapp.service.local.AlbumsDao;
+import com.challenge.albumlistapp.utils.ConnectivityUtils.OnlineChecker;
+import com.challenge.albumlistapp.utils.SchedulerProvider;
 
 import java.util.List;
 
 import javax.inject.Inject;
 
+import io.reactivex.Completable;
 import io.reactivex.Single;
 
 public class DataManagerImpl implements DataManager {
 
     private final AlbumsService albumsService;
+    private final AlbumsDao albumsDao;
+    private final OnlineChecker onlineChecker;
+    private final SchedulerProvider schedulerProvider;
 
     @Inject
-    public DataManagerImpl(AlbumsService albumsService) {
+    public DataManagerImpl(AlbumsService albumsService, AlbumsDao albumsDao, OnlineChecker onlineChecker, SchedulerProvider schedulerProvider) {
         this.albumsService = albumsService;
+        this.albumsDao = albumsDao;
+        this.onlineChecker = onlineChecker;
+        this.schedulerProvider = schedulerProvider;
     }
 
     @Override
     public Single<List<Album>> getAlbums() {
-        return albumsService.getAlbums();
+        return albumsDao.getAlbums()
+                .flatMap(data -> {
+                    if (onlineChecker.isOnline() && data.isEmpty()) {
+                        return albumsService.getAlbums()
+                                .doOnSuccess(albums -> {
+                                    for (Album album : albums) {
+                                        saveAlbum(album);
+                                    }
+                                });
+                    }
+                    return Single.just(data);
+                });
     }
+
+    private void saveAlbum(Album album) {
+        Completable.fromRunnable(() -> albumsDao.inset(album))
+                .subscribeOn(schedulerProvider.io()).subscribe();
+    }
+
 }

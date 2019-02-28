81ee861eca4f8dabbcbc36d9a5acccf4f5b46c32
Joseph <user.example.com>
2019-02-28 10:53:14 +0000

    set base url and relative url

--- app/src/main/java/com/challenge/albumlistapp/utils/Constants.java
+++ app/src/main/java/com/challenge/albumlistapp/utils/Constants.java
@@ -2,5 +2,6 @@ package com.challenge.albumlistapp.utils;
 
 public class Constants {
 
-    public static final String BASE_URL = "";
+    public static final String BASE_URL = "https://jsonplaceholder.typicode.com/";
+    public static final String RELATIVE_URL = "/albums";
 }

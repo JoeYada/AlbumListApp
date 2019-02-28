0c6dc9f214084604e1500675f3f0bf818a7d3c80
Joseph <user.example.com>
2019-02-28 13:13:57 +0000

    Write Instrumentation tests

--- app/src/androidTest/java/com/challenge/albumlistapp/AlbumsScreenTest.java
+++ app/src/androidTest/java/com/challenge/albumlistapp/AlbumsScreenTest.java
@@ -1,26 +1,55 @@
 package com.challenge.albumlistapp;
 
-import android.content.Context;
-import android.support.test.InstrumentationRegistry;
-import android.support.test.runner.AndroidJUnit4;
+import com.challenge.albumlistapp.view.album_list.AlbumListActivity;
 
+import org.junit.Rule;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static org.junit.Assert.*;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.test.espresso.contrib.RecyclerViewActions;
+import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.hasDescendant;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static com.challenge.albumlistapp.TestUtil.atPosition;
 
 /**
  * Instrumented test, which will execute on an Android device.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
  */
 @RunWith(AndroidJUnit4.class)
-public class ExampleInstrumentedTest {
-    @Test
-    public void useAppContext() {
-        // Context of the app under test.
-        Context appContext = InstrumentationRegistry.getTargetContext();
+@LargeTest
+public class AlbumsScreenTest {
 
-        assertEquals("com.challenge.albumlistapp", appContext.getPackageName());
+    @Rule
+    public ActivityTestRule<AlbumListActivity> albumListActivityActivityTestRule =
+            new ActivityTestRule<>(AlbumListActivity.class);
+
+    // More complex tests should be added as app's complexity rises
+    @Test
+    public void displayItemsInList(){
+        // check if the ListView is visible
+        onView(withId(R.id.album_list_recycler_view)).check(matches(isDisplayed()));
     }
+
+    @Test
+    public void scrollRecyclerViewToBottom() {
+        RecyclerView recyclerView = albumListActivityActivityTestRule.getActivity().findViewById(R.id.album_list_recycler_view);
+        onView(withId(R.id.album_list_recycler_view))
+                .perform(RecyclerViewActions.scrollToPosition(recyclerView.getAdapter().getItemCount() -1));
+    }
+
+    @Test
+    public void checkItemAtPosition() {
+        onView(withId(R.id.album_list_recycler_view))
+                .check(matches(atPosition(2, hasDescendant(withText("User ID:")))));
+    }
+
 }
